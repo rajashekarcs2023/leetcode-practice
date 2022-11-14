1bb1c9b71bc0b72583148ee02bfba46f01a417bc
rajashekarcs2023 <114786116+rajashekarcs2023@users.noreply.github.com>
2022-11-14 17:40:01 -0500

    Time: 1 ms (49.77%), Space: 41.2 MB (17.35%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,14 +1,21 @@
 class Solution {
     public int fib(int n) {
-        return nthFib(n);
+        return nthFib(n, new HashMap<Integer,Integer>());
         
     }
-    private int nthFib(int n){
+    private int nthFib(int n, HashMap<Integer, Integer> memo){
         if(n == 0)
             return 0;
-        else if (n == 1)
+        if (n == 1)
             return 1;
-        else
-            return nthFib(n-1)+nthFib(n-2);
+        int currentKey = n;
+        if(memo.containsKey(currentKey))
+            return memo.get(currentKey);
+        
+        int a = nthFib(n-1, memo);
+        int b = nthFib(n-2, memo);
+        
+        memo.put(currentKey,a+b);
+        return a+b;
     }
 }
