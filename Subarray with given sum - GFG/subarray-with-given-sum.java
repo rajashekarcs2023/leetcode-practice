a6865375bf916627f520ad09ecfb8f9b42fd032f
rajashekarcs2023 <114786116+rajashekarcs2023@users.noreply.github.com>
2022-11-18 07:45:05 -0500

    Added solution - LeetHub

---
+++ Subarray with given sum - GFG/subarray-with-given-sum.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main{
+	static BufferedReader br;
+    static PrintWriter ot;
+    public static void main(String[] args) throws IOException{
+        
+        br = new BufferedReader(new InputStreamReader(System.in));
+        ot = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(br.readLine());
+
+        while(t-->0){
+            
+            String s[] = br.readLine().trim().split(" ");
+            
+            int n = Integer.parseInt(s[0]);
+            int k = Integer.parseInt(s[1]);
+            int a[] = new int[n];
+            s = br.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+                a[i] = Integer.parseInt(s[i]);
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.subarraySum(a, n, k);
+            for(int ii = 0;ii<res.size();ii++)
+                ot.print(res.get(ii) + " ");
+            ot.println();
+        }
+        ot.close();
+    }
+
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+static ArrayList<Integer> subarraySum(int[] arr, int n, int s) 
+    {
+        ArrayList<Integer> indices = new ArrayList<Integer>();
+        long currentSum = 0;
+        int i =0;
+        for(int j=0;j<=arr.length-1;j++){
+            currentSum +=arr[j];
+            
+            while(currentSum>s){
+                currentSum -=arr[i];
+                i++;
+            }
+            if(currentSum == s && i<=j){
+                indices.add(i+1);
+                indices.add(j+1);
+                return indices;
+            }
+        }
+        indices.add(-1);
+        return indices;
+    }
+}
